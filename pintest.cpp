92bde4bbaab8a19ec176cc6e0b7f38a072479b59
Victor Savu <victorsavu3@gmail.com>
2016-10-22 14:51:14 +0200

    add test tools

---
+++ pintest.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <pin.H>
+
+INT32 Usage()
+{
+    std::cerr << "This tool pexecutes an application using Intel Pin" << std::endl;
+    std::cerr << endl << KNOB_BASE::StringKnobSummary() << std::endl;
+    return -1;
+}
+
+
+int main(int argc, char * argv[])
+{
+    PIN_InitSymbols();
+
+    if (PIN_Init(argc, argv)) return Usage();
+
+    PIN_StartProgram();
+
+    return 0;
+}
